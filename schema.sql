e2c066eca82924c9bdc7615bfc55997ab8e381ea
MSF2141 <114106019+MSF2141@users.noreply.github.com>
2022-12-01 18:16:45 -0500

    corrected primary keys for table titles.csv

--- schema.sql
+++ schema.sql
@@ -145,3 +145,27 @@ SELECT * FROM salaries;
 SELECT * FROM dept_emp;
 
 SELECT * FROM titles;
+
+DROP TABLE dept_emp; 
+DROP TABLE titles;
+
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+    PRIMARY KEY (emp_no, title, from_date)
+);
