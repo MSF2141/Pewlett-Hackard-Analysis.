eb6c106923eec32f0185da8f0593ece89dbb43c6
MSF2141 <114106019+MSF2141@users.noreply.github.com>
2022-12-07 14:42:05 -0500

    code for mentorship eligibility added

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -34,3 +34,22 @@ GROUP BY unique_titles.title
 ORDER BY unique_titles.count DESC;
 
 SELECT * FROM retiring_titles;
+
+
+-- Mentorship Eligibility 
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no, 
+	   				employees.first_name,
+	   				employees.last_name,
+	   				employees.birth_date,
+	   			    dept_emp.from_date,
+	   				dept_emp.to_date,
+	   				titles.title 
+INTO mentorship_eligibilty 
+FROM employees
+LEFT JOIN dept_emp 
+ON employees.emp_no = dept_emp.emp_no
+LEFT JOIN titles
+ON employees.emp_no = titles.emp_no
+WHERE (dept_emp.to_date = '9999-01-01')
+AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY employees.emp_no;
